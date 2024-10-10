69525f82742e6047d3a3f9c8649607b85b26f870
Sumit Mahour <sumitmahour24@gmail.com>
2024-10-11 00:10:30 +0530

    project initialized

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+
+
+// configure env
+dotenv.config();
+
+// initialize PORT
+const PORT = process.env.PORT || 4500;
+
+// initialize app
+const app = express();
+
+// test api
+app.get("/", (req, res) => {
+  res.send({
+    success: true,
+    message: "API runnin properly...",
+  });
+});
+
+// error handling using middleware
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "!Something went wrong!";
+
+  res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  });
+});
+
+app.listen(PORT, async () => {
+  console.log("server listening on port " + PORT);
+});
